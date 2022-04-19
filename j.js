237746d8732e3678b70f345049b5ead88ebf02f2
MohamedArafat <mohamed.abedellatif13@gmail.com>
2022-04-19 10:28:20 +0300

    JavascriptFile

---
+++ j.js
@@ -0,0 +1,15 @@
+﻿alert("Hello! I am an alert box!")
+ $(document).ready(function() {              
+	          	  $('#h').hide()
+   
+								   $('#bt1').click(function() {
+									 $('#h').show()
+                   })
+			       
+				    $('#bt').click(function() {
+                          $("#bt").hide()
+                   })
+                         $('#bt1').click(function() {
+									 $('#bt').show()
+                   })
+});
